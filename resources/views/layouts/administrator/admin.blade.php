ae272c632da9f51095a47a24d95f1ebe79e1af35
shughurijr07 <abdumalikiandrew@gmail.com>
2023-04-15 11:58:51 +0300

    update image path

--- resources/views/layouts/administrator/admin.blade.php
+++ resources/views/layouts/administrator/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="shortcut icon" type="image/png" href="/images/icon.png"/>
+    <link rel="shortcut icon" type="image/png" href="{{asset('images/icon.png')}}"/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
